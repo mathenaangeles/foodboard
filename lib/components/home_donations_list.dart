c071c707032de2cd16ffa2b33d9e0551cb116196
Carlos Panganiban <cgpanganiban@up.edu.ph>
2021-03-22 23:47:52 +0800

    Flesh out Card for each donation

--- lib/components/home_donations_list.dart
+++ lib/components/home_donations_list.dart
@@ -12,9 +12,7 @@ class HomeDonationsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var donationsCards = <Widget>[];
-    for (var i = 0; i < 50; i++) {
-      donationsCards.add(Text(i.toString()));
-    }
+    donations.forEach((element) => donationsCards.add(DonationCard(element)));
 
     return Container(
         constraints: BoxConstraints.expand(),
@@ -22,5 +20,82 @@ class HomeDonationsList extends StatelessWidget {
         child: ListView(
           children: donationsCards,
         ));
+  }
+}
+
+class DonationCard extends StatelessWidget {
+  final cardData;
+
+  DonationCard(this.cardData);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+              offset: Offset(4, 0),
+              color: shadow_color,
+              spreadRadius: -13.0,
+              blurRadius: 8.0)
+        ]),
+        child: Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            margin: EdgeInsets.fromLTRB(15, 0, 15, 15),
+            child: Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Donation #" + cardData[0].toString(),
+                          style: style_donation_no,
+                        )),
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          cardData[1],
+                          style: style_donation_name,
+                        )),
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          cardData[2],
+                          style: style_donation_address,
+                        )),
+                    SizedBox(height: 10.0),
+                    DonationCardFoodTags(cardData[3]),
+                  ],
+                ))));
+  }
+}
+
+class DonationCardFoodTags extends StatelessWidget {
+  final tags;
+
+  DonationCardFoodTags(this.tags);
+
+  @override
+  Widget build(BuildContext context) {
+    var foodTags = <Widget>[];
+    tags.forEach((element) => foodTags.add(Container(
+        padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+        margin: EdgeInsets.fromLTRB(0, 0, 8, 0),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: text_green,
+            width: 1.5,
+          ),
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: Text(
+          element,
+          style: style_donation_food_tag,
+        ))));
+
+    return Row(
+      children: foodTags,
+    );
   }
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
+const text_green = Color(0xFF3CBA9E);
 const light_green = Color(0xFF67E093);
 const dark_green = Color(0xFF3CBA9E);
 const dark_grey = Color(0xFF83859D);
 
+const shadow_color = Color(0x33343645);
+
 const header_background_color = Colors.white;
-const cards_background_color = Color(0xF8F7FC);
+const cards_background_color = Color(0xFFF8F7FC);
 
 const header_text_size = 15.0;
 const header_tabs_text_size = 12.0;
 
 const header_title_color = Color(0xFF5E6080);
 const header_item_color = Color(0xFF83859D);
+
+const donation_address_color = Color(0xFFB1B6BF);
 
 const style_header_title = TextStyle(
     color: header_title_color,
@@ -27,3 +32,15 @@ const style_header_tabs = TextStyle(
     color: header_item_color,
     fontSize: header_tabs_text_size,
     fontWeight: FontWeight.w600);
+
+const style_donation_no = TextStyle(
+    color: header_item_color, fontSize: 13.0, fontWeight: FontWeight.w600);
+
+const style_donation_name =
+    TextStyle(color: text_green, fontSize: 20.0, fontWeight: FontWeight.w600);
+
+const style_donation_address = TextStyle(
+    color: donation_address_color, fontSize: 13.0, fontWeight: FontWeight.w600);
+
+const style_donation_food_tag =
+    TextStyle(color: text_green, fontSize: 13.0, fontWeight: FontWeight.w600);
