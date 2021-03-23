0698394a6fb697381c2862e11618d34066d10ced
Carlos Panganiban <cgpanganiban@up.edu.ph>
2021-03-24 00:20:01 +0800

    Add logout functionality on title bar press

--- lib/components/home_title_bar.dart
+++ lib/components/home_title_bar.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:foodboard/constants.dart';
 
+import 'package:provider/provider.dart';
+import 'package:foodboard/utils/auth_service.dart';
+import 'package:foodboard/auth/login.dart';
+
 class HomeTitleBar extends StatelessWidget {
   final String text;
   final Function press;
   final Gradient gradient = LinearGradient(colors: <Color>[
-            light_green,
-            dark_green,
-          ], begin: Alignment.topCenter, end: Alignment.bottomCenter);
-  
+    light_green,
+    dark_green,
+  ], begin: Alignment.topCenter, end: Alignment.bottomCenter);
+
   HomeTitleBar({
     Key key,
     this.text,
@@ -19,15 +23,26 @@ class HomeTitleBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-            child: Text(text,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700))),
-        decoration: BoxDecoration(gradient: gradient),
-        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-        height: MediaQuery.of(context).padding.top + barHeight);
+    return GestureDetector(
+        onTap: () {
+          context.read<AuthenticationService>().signOut();
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Login(),
+            ),
+            (route) => false,
+          );
+        },
+        child: Container(
+            child: Center(
+                child: Text(text,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700))),
+            decoration: BoxDecoration(gradient: gradient),
+            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+            height: MediaQuery.of(context).padding.top + barHeight));
   }
 }
