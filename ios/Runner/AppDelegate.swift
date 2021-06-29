e02439db0158134932d164bcf4babe972628dc23
Carlos Panganiban <cgpanganiban@up.edu.ph>
2021-06-30 00:11:08 +0800

    Add boilerplate maps code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBxL7G21Oa0X2i15LCwhG9YHxLDFg4F2AA")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
